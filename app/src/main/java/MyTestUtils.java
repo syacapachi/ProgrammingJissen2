69c61568d5d746519445e57944ad30d25fa876b0
syacapachi <mountainside200@gmail.com>
2026-04-16 11:24:05 +0900

    Javaのバージョン変更11->17

--- app/src/main/java/MyTestUtils.java
+++ app/src/main/java/MyTestUtils.java
@@ -3,11 +3,14 @@ public class MyTestUtils {
         return a + b;
     }
     public static <T extends Number> T Add(T a,T b){
-        if(((Integer) a) != null){
-            return  T.cast(a.intValue() + b.intValue());
+        if(a instanceof  Integer i && b instanceof  Integer k){
+            return (T) Integer.valueOf(i + k);
+        }
+        else if(a instanceof Float fa && b instanceof  Float fb){
+            return (T) Double.valueOf(fa + fb);
         }
         else{
-            return T.cast(0);
+            return  (T)null;
         }
     }
 }
